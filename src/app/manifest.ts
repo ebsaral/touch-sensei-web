6fea2cf7aed2a8edce5f06ca1aee0a0638ccd76b
ebsaral <eminbugrasaral@me.com>
2024-09-29 20:30:49 +0300

    Manifest update

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,8 +3,8 @@ import type { MetadataRoute } from 'next'
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Touch Sensei',
-    short_name: 'Touch Sensei',
-    description: 'Touch Sensei web',
+    short_name: 'Touch Sensei mobile game',
+    description: 'Touch Sensei is a mobile game to enhance and sync your senses',
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
