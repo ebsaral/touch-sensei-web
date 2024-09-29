3db7440fb82162286f4880acc620c38f68c35875
ebsaral <eminbugrasaral@me.com>
2024-09-29 20:49:36 +0300

    add coming soon

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -71,6 +71,7 @@ export default function Home({params: {locale}}: {
           {'Touch Sensei'}
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          {t("HomePage.soon")}
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center gap-2 h-10 sm:h-12 px-4 sm:px-5"
             href="/"
