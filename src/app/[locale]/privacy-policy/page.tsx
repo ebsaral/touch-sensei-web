76c76303e2f867911f4e992446cd07b20c324cf9
ebsaral <eminbugrasaral@me.com>
2024-10-01 17:25:48 +0300

    language menu for sub pages

--- src/app/[locale]/privacy-policy/page.tsx
+++ src/app/[locale]/privacy-policy/page.tsx
@@ -26,12 +26,36 @@ export default function Home({params: {locale}}: {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <Image
+              aria-hidden
+              src="https://nextjs.org/icons/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
+          />
           <Link
             className={`flex items-center gap-2 hover:underline hover:underline-offset-4` + `${locale === 'en' && '  font-bold'}`}
-            href="/"
+            href="/privacy-policy"
+            locale="en"
           >
-            {t("Link.home")}
+            English
+          </Link>
+          {" | "}
+          <Link
+            className={`flex items-center gap-2 hover:underline hover:underline-offset-4` + `${locale === 'tr' && '  font-bold'}`}
+            href="/privacy-policy"
+            locale="tr"
+          >
+            Türkçe
+          </Link>
+          {" | "}
+          <Link
+            className={`flex items-center gap-2 hover:underline hover:underline-offset-4` + `${locale === 'de' && '  font-bold'}`}
+            href="/privacy-policy"
+            locale="de"
+          >
+            Deutsch
           </Link>
         </div>
         <Image
